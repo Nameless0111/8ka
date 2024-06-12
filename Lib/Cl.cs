c44abd1f193896902429e9aebb68311d14d6312e
Nameless0111 <isip_p.a.bilashenko@mpt.ru>
2024-06-12 15:48:35 +0300

    Добавьте файлы проекта.

---
+++ Lib/Cl.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+namespace Lib
+{
+    public static class Cl<T>
+    {
+        public static string SerializeObject(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static T DeserializeObject<T>(string json)
+        {
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib;
+namespace WpfApp2
+{
+     public class Info
+    {
+        public string info { get; set; }
+}
+public partial class MainWindow : Window
+    {
+        public string json;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TbxC(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_1(object sender, RoutedEventArgs e)
+        {
+            Info info = Cl<Info>.DeserializeObject<Info>(json);
+            Tbx.Text = info.info;
+        }
+
+        private void Button_2(object sender, RoutedEventArgs e)
+        {
+            Info info = new Info();
+            info.info = Tbx.Text;
+            json = Cl<Info>.SerializeObject(info);
+        }
+    }
+}
